17b50cd331a0c65dab568a2e84c1ad72c759ebc6
Girish Ganesan <girishg0110@gmail.com>
2023-06-01 22:29:32 +0000

    fixed Claude API calls

--- server/server.py
+++ server/server.py
@@ -1,15 +1,16 @@
-import openai
 from dotenv import load_dotenv
 import streamlit as st
-#from flask import Flask
+import anthropic
+# from flask import Flask
 import os
 
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
+claude = anthropic.Client(anthropic_api_key)
 
-#app = Flask(__name__)
+# app = Flask(__name__)
 
-# Stages: 
+# Stages:
 #   (1) Console App
 #   (2) Streamlit Deployment
 #   (3) Stateful API & Frontend *
@@ -17,14 +18,14 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 # Command Loop: {EN <-> FR}
-    # START. 
-    # The LLM writes a sentence. The user translates. 
-    # The LLM judges. The user writes a sentence. The LLM judges. 
-    # Goto START. 
+# START.
+# The LLM writes a sentence. The user translates.
+# The LLM judges. The user writes a sentence. The LLM judges.
+# Goto START.
 # Need to call for (1) story completion and (2) judging -> extract poorly used vocab
-# 1) POST /story-completion/ {story_history} => story_history[-1] = last user sentence 
-    # : return {story_history}* , translation of story_history[-1], feedback on previous sentence
-# Run two agents. One for telling the story and exchanging sentences. One acts on user sentences and gives feedback, also extracts poorly used words. 
+# 1) POST /story-completion/ {story_history} => story_history[-1] = last user sentence
+# : return {story_history}* , translation of story_history[-1], feedback on previous sentence
+# Run two agents. One for telling the story and exchanging sentences. One acts on user sentences and gives feedback, also extracts poorly used words.
 # Will eventually store conversations in a database and just send conversation_id to the user
 
 class FeedbackAgent:
@@ -34,51 +35,48 @@ class FeedbackAgent:
         self.target_language = target_language
 
     def get_feedback(self, user_sentence):
-        prompt_setup = [
-            {"role" : "system", "content" : "You are a {target_language} language tutor. I will write a sentence in {target_language} and you will give feedback on the correctness of that sentence."},
-            {"role" : "user", "content" : user_sentence}
-        ]
-        gpt_response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=prompt_setup
+        prompt_setup = f"""{anthropic.HUMAN_PROMPT} You are a {target_language} language tutor. 
+            I will write a sentence in {target_language} and you will give feedback on the correctness 
+            of that sentence:\n{user_sentence}{anthropic.AI_PROMPT}"""
+        claude_response = claude.completion(
+            prompt=prompt_setup,
+            max_tokens_to_sample=300,
+            model="claude-v1",
         )
-        feedback = gpt_response['choices'][0]['message']['content']
+        feedback = claude_response['completion']
         return feedback
+
 
 class StoryAgent:
     system_message = ""
-    story_history = []
+    story_history = ""
 
     def __init__(self, target_language):
-        self.system_message = f"You are a {target_language} language tutor. We will play a collaborative story-writing game where we take turns writing one sentence each of a story. "
-        self.story_history.append({
-            "role" : "system",
-            "content" : self.system_message
-        })
-    
-    def send_message(self, new_message):
-        self.story_history.append({
-            "role" : "user", 
-            "content" : new_message
-        })
+        self.system_message = f"{anthropic.HUMAN_PROMPT} You are a {target_language} language tutor. Continue the following story with one sentence:\n"
+        self.story_history = ""
 
-        gpt_response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=self.story_history
+    def send_message(self, new_message):
+        self.story_history += new_message 
+        prompt_input = f"""{self.system_message}{self.story_history} {anthropic.AI_PROMPT}"""
+
+        claude_response = claude.completion(
+            prompt=prompt_input, 
+            max_tokens_to_sample=300,
+            model="claude-v1"
         )
-        assistant_response = gpt_response['choices'][0]['message']['content']
-        self.story_history.append({
-            "role": "assistant", 
-            "content": assistant_response
-        })
+        #print(claude_response)
+        assistant_response = claude_response['completion']
+        self.story_history += ' ' + assistant_response
 
         return assistant_response
+
 
 def display_conversation(convo):
     for exchange in convo:
         student, tutor = exchange
         st.write(student)
         st.write(tutor)
+
 
 if __name__ == "__main__":
     st.title("Lisan")
@@ -88,12 +86,8 @@ if __name__ == "__main__":
     i = 0
     new_message = st.sidebar.text_input("You: ", i)
     submit_message = st.sidebar.button("Add Sentence")
-    conversation = []
     if submit_message:
         response = storywriter.send_message(new_message)
-        conversation.append([new_message, response])
-        #feedback = tutor.get_feedback(new_message)
-        #st.write(f"Feedback: {feedback}")
-        
-    display_conversation(conversation)
-    #app.run(debug=True)
+
+    st.write(storywriter.story_history)
+    # app.run(debug=True)
