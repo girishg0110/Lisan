9b5cdff9ecf4408026cacd3b499a77534ed6f054
girishg0110 <girishg0110@gmail.com>
2023-06-02 19:08:19 -0400

    spaced out emojis on home page

--- Home.py
+++ Home.py
@@ -18,10 +18,16 @@ st.markdown(
     Lisan (pron. lee-saan) uses collaborative storytelling to fill the market gap in intermediate-level language learning services. Users will develop professional competency in an international language (like English, French, Spanish, etc...) by writing a story with Lisan.
 
     🧠 Receive immediate feedback on your sentences.
+
     👩‍🏫 Tailor Lisan to match your CEFR language level.
+
     🎤 Hear Lisan speak in your language.
+
     ⛏️ Get a word-level breakdown of all sentences in the story.
+
     📂 Export flashcards for use on platforms like Anki and Quizlet.
+
     📈 View analytics on a personalized dashboard.
+    
     """
 )
